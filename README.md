8a2e883032975279398d38ca266f876c3729d535
VeqsaGit <virgiliodalessi@gmail.com>
2021-08-02 16:52:55 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Toverland
+
+See the current queues
+in themepark toverland
+
+## Installation
+
+How to install the toverland package using npm
+
+```bash
+npm install toverland
+```
+
+## Usage
+
+How to use the toverland package
+
+```javascript
+const toverland = require('toverland')
+```
+```javascript
+let queues = toverland.queue()
+console.log(queues)
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+
+function queue() {
+    const html = await axios.get('https://www.looopings.nl/wachten/toverland');
+    const dom = new JSDOM(html.data);
+
+    let now = new Date().getTime()
+
+    let toverlandtemp = {
+        [now]: {
+        }
+    }
+
+    let x = dom.window.document.getElementsByClassName('name')
+
+    for (let i = 0; i < x.length; i++) { 
+
+        let waittime = (/^\d+$/.test(dom.window.document.getElementsByClassName('waittime')[i]?.innerHTML)) ? parseInt(dom.window.document.getElementsByClassName('waittime')[i]?.innerHTML) : 0 
+
+        toverlandtemp[now][dom.window.document.getElementsByClassName('name')[i].innerHTML] = waittime
+    }
+    return toverlandtemp
+}
+
+module.exports.toverland = queue;
